f6c9802080ad5e7dddceb45c5b117999e0bc9be2
sachin3.yadav <sachin3.yadav@paytmmoney.com>
2022-08-09 17:45:20 +0530

    update unit test coverage

--- src/main/java/com/paytmmoney/equities/pmclient/response/UserMeta.java
+++ src/main/java/com/paytmmoney/equities/pmclient/response/UserMeta.java
@@ -2,10 +2,12 @@ package com.paytmmoney.equities.pmclient.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
