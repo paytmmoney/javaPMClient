39595b020f26889232969f0223f004c463d8e6dc
Abhishek61 <abhishek61.kumar@paytmmoney.com>
2023-03-14 17:45:21 +0530

    used builder property while creating tick

--- src/main/java/com/paytmmoney/equities/pmclient/ticker/WebSocketClient.java
+++ src/main/java/com/paytmmoney/equities/pmclient/ticker/WebSocketClient.java
@@ -330,14 +330,15 @@ public class WebSocketClient {
      * @param position current position of packet to be processed
      */
     private void processIndexLtpPacket(ArrayList<Tick> ticks, ByteBuffer packet, int position) {
-        Tick tick = new Tick();
-        tick.setLastTradedPrice(packet.getFloat(position + LTP_OFFSET));
-        tick.setLastUpdatedTime(EpochConverterUtil.epochConverter(packet.getInt(position + INDEX_LTP_LUT_OFFSET)));
-        tick.setSecurityId(packet.getInt(position + INDEX_LTP_SECURITY_ID_OFFSET));
-        tick.setTradable(packet.get(position + INDEX_LTP_TRADABLE_OFFSET));
-        tick.setMode(packet.get(position + INDEX_LTP_MODETYPE_OFFSET));
-        tick.setChangeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + INDEX_LTP_CHANGE_ABSOLUTE_OFFSET))));
-        tick.setChangePercent(packet.getFloat(position + INDEX_LTP_CHANGE_PERCENT_OFFSET));
+        Tick tick = Tick.builder()
+                .lastTradedPrice(packet.getFloat(position + LTP_OFFSET))
+                .lastUpdatedTime(EpochConverterUtil.epochConverter(packet.getInt(position + INDEX_LTP_LUT_OFFSET)))
+                .securityId(packet.getInt(position + INDEX_LTP_SECURITY_ID_OFFSET))
+                .tradable(packet.get(position + INDEX_LTP_TRADABLE_OFFSET))
+                .mode(packet.get(position + INDEX_LTP_MODETYPE_OFFSET))
+                .changeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + INDEX_LTP_CHANGE_ABSOLUTE_OFFSET))))
+                .changePercent(packet.getFloat(position + INDEX_LTP_CHANGE_PERCENT_OFFSET))
+                .build();
         ticks.add(tick);
     }
 
@@ -349,19 +350,20 @@ public class WebSocketClient {
      * @param position current position of packet to be processed
      */
     private void processIndexQuotePacket(ArrayList<Tick> ticks, ByteBuffer packet, int position) {
-        Tick tick = new Tick();
-        tick.setLastTradedPrice(packet.getFloat(position + LTP_OFFSET));
-        tick.setSecurityId(packet.getInt(position + INDEX_QUOTE_SECURITY_ID_OFFSET));
-        tick.setTradable(packet.get(position + INDEX_QUOTE_TRADABLE_OFFSET));
-        tick.setMode(packet.get(position + INDEX_QUOTE_MODETYPE_OFFSET));
-        tick.setOpen(packet.getFloat(position + INDEX_QUOTE_OPEN_OFFSET));
-        tick.setClose(packet.getFloat(position + INDEX_QUOTE_CLOSE_OFFSET));
-        tick.setHigh(packet.getFloat(position + INDEX_QUOTE_HIGH_OFFSET));
-        tick.setLow(packet.getFloat(position + INDEX_QUOTE_LOW_OFFSET));
-        tick.setChangePercent(packet.getFloat(position + INDEX_QUOTE_CHANGE_PERCENT_OFFSET));
-        tick.setChangeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + INDEX_QUOTE_CHANGE_ABSOLUTE_OFFSET))));
-        tick.setFiftyTwoWeekHigh(packet.getFloat(position + INDEX_QUOTE_52_WEEK_HIGH_OFFSET));
-        tick.setFiftyTwoWeekLow(packet.getFloat(position + INDEX_QUOTE_52_WEEK_LOW_OFFSET));
+        Tick tick = Tick.builder()
+                .lastTradedPrice(packet.getFloat(position + LTP_OFFSET))
+                .securityId(packet.getInt(position + INDEX_QUOTE_SECURITY_ID_OFFSET))
+                .tradable(packet.get(position + INDEX_QUOTE_TRADABLE_OFFSET))
+                .mode(packet.get(position + INDEX_QUOTE_MODETYPE_OFFSET))
+                .open(packet.getFloat(position + INDEX_QUOTE_OPEN_OFFSET))
+                .close(packet.getFloat(position + INDEX_QUOTE_CLOSE_OFFSET))
+                .high(packet.getFloat(position + INDEX_QUOTE_HIGH_OFFSET))
+                .low(packet.getFloat(position + INDEX_QUOTE_LOW_OFFSET))
+                .changePercent(packet.getFloat(position + INDEX_QUOTE_CHANGE_PERCENT_OFFSET))
+                .changeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + INDEX_QUOTE_CHANGE_ABSOLUTE_OFFSET))))
+                .fiftyTwoWeekHigh(packet.getFloat(position + INDEX_QUOTE_52_WEEK_HIGH_OFFSET))
+                .fiftyTwoWeekLow(packet.getFloat(position + INDEX_QUOTE_52_WEEK_LOW_OFFSET))
+                .build();
         ticks.add(tick);
     }
 
@@ -373,18 +375,19 @@ public class WebSocketClient {
      * @param position current position of packet to be processed
      */
     private void processIndexFullPacket(ArrayList<Tick> ticks, ByteBuffer packet, int position) {
-        Tick tick = new Tick();
-        tick.setLastTradedPrice(packet.getFloat(position + LTP_OFFSET));
-        tick.setSecurityId(packet.getInt(position + INDEX_FULL_SECURITY_ID_OFFSET));
-        tick.setTradable(packet.get(position + INDEX_FULL_TRADABLE_OFFSET));
-        tick.setMode(packet.get(position + INDEX_FULL_MODETYPE_OFFSET));
-        tick.setOpen(packet.getFloat(position + INDEX_FULL_OPEN_OFFSET));
-        tick.setClose(packet.getFloat(position + INDEX_FULL_CLOSE_OFFSET));
-        tick.setHigh(packet.getFloat(position + INDEX_FULL_HIGH_OFFSET));
-        tick.setLow(packet.getFloat(position + INDEX_FULL_LOW_OFFSET));
-        tick.setChangePercent(packet.getFloat(position + INDEX_FULL_CHANGE_PERCENT_OFFSET));
-        tick.setChangeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + INDEX_FULL_CHANGE_ABSOLUTE_OFFSET))));
-        tick.setLastUpdatedTime(EpochConverterUtil.epochConverter(packet.getInt(position + INDEX_FULL_LUT_OFFSET)));
+        Tick tick = Tick.builder()
+                .lastTradedPrice(packet.getFloat(position + LTP_OFFSET))
+                .securityId(packet.getInt(position + INDEX_FULL_SECURITY_ID_OFFSET))
+                .tradable(packet.get(position + INDEX_FULL_TRADABLE_OFFSET))
+                .mode(packet.get(position + INDEX_FULL_MODETYPE_OFFSET))
+                .open(packet.getFloat(position + INDEX_FULL_OPEN_OFFSET))
+                .close(packet.getFloat(position + INDEX_FULL_CLOSE_OFFSET))
+                .high(packet.getFloat(position + INDEX_FULL_HIGH_OFFSET))
+                .low(packet.getFloat(position + INDEX_FULL_LOW_OFFSET))
+                .changePercent(packet.getFloat(position + INDEX_FULL_CHANGE_PERCENT_OFFSET))
+                .changeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + INDEX_FULL_CHANGE_ABSOLUTE_OFFSET))))
+                .lastUpdatedTime(EpochConverterUtil.epochConverter(packet.getInt(position + INDEX_FULL_LUT_OFFSET)))
+                .build();
         ticks.add(tick);
     }
 
@@ -396,14 +399,15 @@ public class WebSocketClient {
      * @param position current position of packet to be processed
      */
     private void processLtpPacket(ArrayList<Tick> ticks, ByteBuffer packet, int position) {
-        Tick tick = new Tick();
-        tick.setLastTradedPrice(packet.getFloat(position + LTP_OFFSET));
-        tick.setLastTradedTime(EpochConverterUtil.epochConverter(packet.getInt(position + LTP_LTT_OFFSET)));
-        tick.setSecurityId(packet.getInt(position + LTP_SECURITY_ID_OFFSET));
-        tick.setTradable(packet.get(position + LTP_TRADABLE_OFFSET));
-        tick.setMode(packet.get(position + LTP_MODETYPE_OFFSET));
-        tick.setChangeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + LTP_CHANGE_ABSOLUTE_OFFSET))));
-        tick.setChangePercent(packet.getFloat(position + LTP_CHANGE_PERCENT_OFFSET));
+        Tick tick = Tick.builder()
+                .lastTradedPrice(packet.getFloat(position + LTP_OFFSET))
+                .lastTradedTime(EpochConverterUtil.epochConverter(packet.getInt(position + LTP_LTT_OFFSET)))
+                .securityId(packet.getInt(position + LTP_SECURITY_ID_OFFSET))
+                .tradable(packet.get(position + LTP_TRADABLE_OFFSET))
+                .mode(packet.get(position + LTP_MODETYPE_OFFSET))
+                .changeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + LTP_CHANGE_ABSOLUTE_OFFSET))))
+                .changePercent(packet.getFloat(position + LTP_CHANGE_PERCENT_OFFSET))
+                .build();
         ticks.add(tick);
     }
 
@@ -415,25 +419,26 @@ public class WebSocketClient {
      * @param position current position of packet to be processed
      */
     private void processQuotePacket(ArrayList<Tick> ticks, ByteBuffer packet, int position) {
-        Tick tick = new Tick();
-        tick.setLastTradedPrice(packet.getFloat(position + LTP_OFFSET));
-        tick.setLastTradedTime(EpochConverterUtil.epochConverter(packet.getInt(position + QUOTE_LTT_OFFSET)));
-        tick.setSecurityId(packet.getInt(position + QUOTE_SECURITY_ID_OFFSET));
-        tick.setTradable(packet.get(position + QUOTE_TRADABLE_OFFSET));
-        tick.setMode(packet.get(position + QUOTE_MODETYPE_OFFSET));
-        tick.setLastTradedQuantity(packet.getInt(position + QUOTE_LAST_TRADED_QUANTITY_OFFSET));
-        tick.setAverageTradedPrice(packet.getFloat(position + QUOTE_AVG_TRADED_PRICE_OFFSET));
-        tick.setVolumeTraded(packet.getInt(position + QUOTE_VOLUME_OFFSET));
-        tick.setTotalBuyQuantity(packet.getInt(position + QUOTE_TOTAL_BUY_QUANTITY_OFFSET));
-        tick.setTotalSellQuantity(packet.getInt(position + QUOTE_TOTAL_SELL_QUANTITY_OFFSET));
-        tick.setOpen(packet.getFloat(position + QUOTE_OPEN_OFFSET));
-        tick.setClose(packet.getFloat(position + QUOTE_CLOSE_OFFSET));
-        tick.setHigh(packet.getFloat(position + QUOTE_HIGH_OFFSET));
-        tick.setLow(packet.getFloat(position + QUOTE_LOW_OFFSET));
-        tick.setChangePercent(packet.getFloat(position + QUOTE_CHANGE_PERCENT_OFFSET));
-        tick.setChangeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + QUOTE_CHANGE_ABSOLUTE_OFFSET))));
-        tick.setFiftyTwoWeekHigh(packet.getFloat(position + QUOTE_52_WEEK_HIGH_OFFSET));
-        tick.setFiftyTwoWeekLow(packet.getFloat(position + QUOTE_52_WEEK_LOW_OFFSET));
+        Tick tick = Tick.builder()
+                .lastTradedPrice(packet.getFloat(position + LTP_OFFSET))
+                .lastTradedTime(EpochConverterUtil.epochConverter(packet.getInt(position + QUOTE_LTT_OFFSET)))
+                .securityId(packet.getInt(position + QUOTE_SECURITY_ID_OFFSET))
+                .tradable(packet.get(position + QUOTE_TRADABLE_OFFSET))
+                .mode(packet.get(position + QUOTE_MODETYPE_OFFSET))
+                .lastTradedQuantity(packet.getInt(position + QUOTE_LAST_TRADED_QUANTITY_OFFSET))
+                .averageTradedPrice(packet.getFloat(position + QUOTE_AVG_TRADED_PRICE_OFFSET))
+                .volumeTraded(packet.getInt(position + QUOTE_VOLUME_OFFSET))
+                .totalBuyQuantity(packet.getInt(position + QUOTE_TOTAL_BUY_QUANTITY_OFFSET))
+                .totalSellQuantity(packet.getInt(position + QUOTE_TOTAL_SELL_QUANTITY_OFFSET))
+                .open(packet.getFloat(position + QUOTE_OPEN_OFFSET))
+                .close(packet.getFloat(position + QUOTE_CLOSE_OFFSET))
+                .high(packet.getFloat(position + QUOTE_HIGH_OFFSET))
+                .low(packet.getFloat(position + QUOTE_LOW_OFFSET))
+                .changePercent(packet.getFloat(position + QUOTE_CHANGE_PERCENT_OFFSET))
+                .changeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + QUOTE_CHANGE_ABSOLUTE_OFFSET))))
+                .fiftyTwoWeekHigh(packet.getFloat(position + QUOTE_52_WEEK_HIGH_OFFSET))
+                .fiftyTwoWeekLow(packet.getFloat(position + QUOTE_52_WEEK_LOW_OFFSET))
+                .build();
         ticks.add(tick);
     }
 
@@ -445,41 +450,42 @@ public class WebSocketClient {
      * @param position current position of packet to be processed
      */
     private void processFullPacket(ArrayList<Tick> ticks, ByteBuffer packet, int position) {
-        Tick tick = new Tick();
         ArrayList<DepthPacket> depthPacketList = new ArrayList<>();
         for (int i = 0; i < DEPTH_PACKETS_COUNT; i++) {
-            DepthPacket depthPacket = new DepthPacket();
-            depthPacket.setPacketNo(i + 1);
-            depthPacket.setBuyQuantity(packet.getInt(position + DEPTH_BUY_QUANTITY_OFFSET + (i * DEPTH_PACKET_SIZE)));
-            depthPacket.setSellQuantity(packet.getInt(position + DEPTH_SELL_QUANTITY_OFFSET + (i * DEPTH_PACKET_SIZE)));
-            depthPacket.setBuyOrder(packet.getShort(position + DEPTH_BUY_ORDER_OFFSET + (i * DEPTH_PACKET_SIZE)));
-            depthPacket.setSellOrder(packet.getShort(position + DEPTH_SELL_ORDER_OFFSET + (i * DEPTH_PACKET_SIZE)));
-            depthPacket.setBuyPrice(packet.getFloat(position + DEPTH_BUY_PRICE_OFFSET + (i * DEPTH_PACKET_SIZE)));
-            depthPacket.setSellPrice(packet.getFloat(position + DEPTH_SELL_PRICE_OFFSET + (i * DEPTH_PACKET_SIZE)));
+            DepthPacket depthPacket = DepthPacket.builder()
+                    .packetNo(i + 1)
+                    .buyQuantity(packet.getInt(position + DEPTH_BUY_QUANTITY_OFFSET + (i * DEPTH_PACKET_SIZE)))
+                    .sellQuantity(packet.getInt(position + DEPTH_SELL_QUANTITY_OFFSET + (i * DEPTH_PACKET_SIZE)))
+                    .buyOrder(packet.getShort(position + DEPTH_BUY_ORDER_OFFSET + (i * DEPTH_PACKET_SIZE)))
+                    .sellOrder(packet.getShort(position + DEPTH_SELL_ORDER_OFFSET + (i * DEPTH_PACKET_SIZE)))
+                    .buyPrice(packet.getFloat(position + DEPTH_BUY_PRICE_OFFSET + (i * DEPTH_PACKET_SIZE)))
+                    .sellPrice(packet.getFloat(position + DEPTH_SELL_PRICE_OFFSET + (i * DEPTH_PACKET_SIZE)))
+                    .build();
             depthPacketList.add(depthPacket);
         }
-
-        tick.setMbpRowPacket(depthPacketList);
-        tick.setLastTradedPrice(packet.getFloat(position + FULL_LTP_OFFSET));
-        tick.setLastTradedTime(EpochConverterUtil.epochConverter(packet.getInt(position + FULL_LTT_OFFSET)));
-        tick.setSecurityId(packet.getInt(position + FULL_SECURITY_ID_OFFSET));
-        tick.setTradable(packet.get(position + FULL_TRADABLE_OFFSET));
-        tick.setMode(packet.get(position + FULL_MODETYPE_OFFSET));
-        tick.setLastTradedQuantity(packet.getInt(position + FULL_LAST_TRADED_QUANTITY_OFFSET));
-        tick.setAverageTradedPrice(packet.getFloat(position + FULL_AVG_TRADED_PRICE_OFFSET));
-        tick.setVolumeTraded(packet.getInt(position + FULL_VOLUME_OFFSET));
-        tick.setTotalBuyQuantity(packet.getInt(position + FULL_TOTAL_BUY_QUANTITY_OFFSET));
-        tick.setTotalSellQuantity(packet.getInt(position + FULL_TOTAL_SELL_QUANTITY_OFFSET));
-        tick.setOpen(packet.getFloat(position + FULL_OPEN_OFFSET));
-        tick.setClose(packet.getFloat(position + FULL_CLOSE_OFFSET));
-        tick.setHigh(packet.getFloat(position + FULL_HIGH_OFFSET));
-        tick.setLow(packet.getFloat(position + FULL_LOW_OFFSET));
-        tick.setChangePercent(packet.getFloat(position + FULL_CHANGE_PERCENT_OFFSET));
-        tick.setChangeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + FULL_CHANGE_ABSOLUTE_OFFSET))));
-        tick.setFiftyTwoWeekHigh(packet.getFloat(position + FULL_52_WEEK_HIGH_OFFSET));
-        tick.setFiftyTwoWeekLow(packet.getFloat(position + FULL_52_WEEK_LOW_OFFSET));
-        tick.setOi(packet.getInt(position + FULL_OI_OFFSET));
-        tick.setOiChange(packet.getInt(position + FULL_CHANGE_OI_OFFSET));
+        Tick tick = Tick.builder()
+                .mbpRowPacket(depthPacketList)
+                .lastTradedPrice(packet.getFloat(position + FULL_LTP_OFFSET))
+                .lastTradedTime(EpochConverterUtil.epochConverter(packet.getInt(position + FULL_LTT_OFFSET)))
+                .securityId(packet.getInt(position + FULL_SECURITY_ID_OFFSET))
+                .tradable(packet.get(position + FULL_TRADABLE_OFFSET))
+                .mode(packet.get(position + FULL_MODETYPE_OFFSET))
+                .lastTradedQuantity(packet.getInt(position + FULL_LAST_TRADED_QUANTITY_OFFSET))
+                .averageTradedPrice(packet.getFloat(position + FULL_AVG_TRADED_PRICE_OFFSET))
+                .volumeTraded(packet.getInt(position + FULL_VOLUME_OFFSET))
+                .totalBuyQuantity(packet.getInt(position + FULL_TOTAL_BUY_QUANTITY_OFFSET))
+                .totalSellQuantity(packet.getInt(position + FULL_TOTAL_SELL_QUANTITY_OFFSET))
+                .open(packet.getFloat(position + FULL_OPEN_OFFSET))
+                .close(packet.getFloat(position + FULL_CLOSE_OFFSET))
+                .high(packet.getFloat(position + FULL_HIGH_OFFSET))
+                .low(packet.getFloat(position + FULL_LOW_OFFSET))
+                .changePercent(packet.getFloat(position + FULL_CHANGE_PERCENT_OFFSET))
+                .changeAbsolute(Float.parseFloat(decimalFormat.format(packet.getFloat(position + FULL_CHANGE_ABSOLUTE_OFFSET))))
+                .fiftyTwoWeekHigh(packet.getFloat(position + FULL_52_WEEK_HIGH_OFFSET))
+                .fiftyTwoWeekLow(packet.getFloat(position + FULL_52_WEEK_LOW_OFFSET))
+                .oi(packet.getInt(position + FULL_OI_OFFSET))
+                .oiChange(packet.getInt(position + FULL_CHANGE_OI_OFFSET))
+                .build();
         ticks.add(tick);
     }
 }
